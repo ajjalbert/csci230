cf10dfa15271ad6cbbc1b6e3238e4b8a8da67e91
000000coffee <kottwitz.matt@gmail.com>
2017-03-02 11:51:47 -0800

    new folder

--- GuitarString.java
+++
@@ -1,8 +0,0 @@
-public class GuitarString {
-   public GuitarString(double frequency) { }
-   public GuitarString(double[] init) {}
-   public void pluck() {}
-   public void tic() {}
-   public double sample() {}
-   public int time() {}
-}
