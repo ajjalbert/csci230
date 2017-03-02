a9f1f484592dd464d4df1247d30bc3e45187148f
000000coffee <kottwitz.matt@gmail.com>
2017-03-01 16:08:56 -0800

    Add files via upload

---
+++ GuitarString.java
@@ -0,0 +1,8 @@
+public class GuitarString {
+   public GuitarString(double frequency) { }
+   public GuitarString(double[] init) {}
+   public void pluck() {}
+   public void tic() {}
+   public double sample() {}
+   public int time() {}
+}
